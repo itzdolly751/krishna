15cbdda9823f0c5af854d06bb73e6ef6bec8803d
itzdolly751 <kirshk637@gmail.com>
2024-11-25 23:25:00 -0800

    Add files via upload

---
+++ d.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    sum = num1 + num2;
+
+    printf("The sum is: %.2f\n", sum);
+    return 0;
+}
+
+
+
+
+
