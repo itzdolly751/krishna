7e7c89ebb5a05df0da700d5ed4e299fec4644334
itzdolly751 <kirshk637@gmail.com>
2024-11-26 23:03:47 -0800

    Add files via upload

---
+++ sd32.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+    };
+
+    // Print the matrix
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
